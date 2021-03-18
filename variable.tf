e539713ba7383a3bb27877c29035d90b0f68e7fa
naresh <agrawal.nar@northeastern.edu>
2021-03-18 17:40:48 -0400

    addding terraform script

---
+++ variable.tf
@@ -0,0 +1,46 @@
+variable aws_region {
+  	default = "us-east-1"
+}
+
+variable profile {
+	default = "dev"
+}
+
+# VPC_cidr
+variable "vpc_cidr" {
+  type = "string"
+}
+
+# Public Subnet_cidrs
+variable "public_cidrs" {
+  type = "list"
+}
+
+# AMI
+variable "ami" {
+  type = "string"
+}
+
+#  Avilable Availibility Zone
+data "aws_availability_zones" "available" {
+
+}
+
+# Database name
+variable "db_name" {
+  type = "string"
+}
+
+# Database user name
+variable "db_user_name" {
+  type = "string"
+}
+
+# Database password
+variable "db_password" {
+  type = "string"
+}
+
+variable "ssh-key" {
+  
+}
